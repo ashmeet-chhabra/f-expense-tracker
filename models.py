9cd6bf5018e1292271e646b609af9a93fd6ac2e8
Ashmeet <ashmeetchhabra111@gmail.com>
2026-02-27 14:54:20 +0530

    created user model

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String, Date
+from sqlalchemy import Column, Integer, String, Date, ForeignKey
+from sqlalchemy.orm import relationship
 from datetime import date
 
 from database import Base
@@ -11,3 +12,15 @@ class ExpenseModel(Base):
     amount = Column(Integer, nullable=False)
     date = Column(Date, default=date.today)
 
+    user_id = Column(Integer, ForeignKey("users.id"))
+    user = relationship("UserModel", back_populates="expenses")
+
+class UserModel(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, default="Guest")
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+
+    expenses = relationship("ExpenseModel", back_populates="user")
