6caa98e5e4fb5332dcf5e23f74f83989e1b860d3
Ashmeet <ashmeetchhabra111@gmail.com>
2026-02-28 06:10:00 +0530

    jwt token

--- auth.py
+++ auth.py
@@ -1,4 +1,10 @@
 from passlib.context import CryptContext
+from jose import jwt, JWTError
+from datetime import datetime, timedelta
+
+SECRET_KEY = "hello-hi-bye-ba-bye"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -7,3 +13,9 @@ def hash_password(password: str):
 
 def verify_password(plain_password: str, hashed_password:str):
     return pwd_context.verify(plain_password, hashed_password)
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
--- routers/users.py
+++ routers/users.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter, Depends, HTTPException, Query
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy import func, extract
 from sqlalchemy.orm import Session
 
 from database import get_db
 from models import UserModel
 from schemas import UserCreate, UserResponse
-from auth import hash_password
+from auth import hash_password, verify_password, create_access_token
 
 router = APIRouter(
     prefix = "/users",
@@ -33,3 +34,21 @@ def register(user: UserCreate, db: Session = Depends(get_db)):
     db.refresh(db_user) 
 
     return db_user
+
+@router.post('/login')
+def login(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    db: Session = Depends(get_db)
+):
+    user = db.query(UserModel).filter(
+        UserModel.email == form_data.username
+    ).first()
+
+    if not user or not verify_password(form_data.password, user.hashed_password):
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+    
+    access_token = create_access_token(
+        {"sub": str(user.id)}
+    )
+
+    return {"access_token": access_token, "token_type": "bearer"}
